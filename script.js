e1d5b3ce25cfd36b95d02731f6f73d871c9edf92
Tony <tomcatbuzz@yahoo.com>
2022-03-15 22:14:53 -0400

    done b/w

--- script.js
+++ script.js
@@ -11,23 +11,52 @@ myImage.addEventListener('load', () => {
 
   ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height)
   const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height)
-  console.log(pixels)
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
 
   let particlesArray = []
-  const numberOfParticles = 2000
+  const numberOfParticles = 5000
 
   let mappedImage = []
+  for(let y = 0; y < canvas.height; y++) {
+    let row = []
+    for(let x = 0; x < canvas.width; x++) {
+      const red = pixels.data[(y * 4 * pixels.width) + (x * 4)]
+      const green = pixels.data[(y * 4 * pixels.width) + (x * 4 + 1)]
+      const blue = pixels.data[(y * 4 * pixels.width) + (x * 4 + 2)]
+      const brightness = calRelBrightness(red, green, blue)
+      const cell = [
+        cellBrightness = brightness,
+      ]
+      row.push(cell)
+    }
+    mappedImage.push(row)
+  }
+  console.log(mappedImage)
+  // calculate relative brightness
+  function calRelBrightness(red, green, blue) {
+    return Math.sqrt(
+      (red * red) * 0.299 +
+      (green * green) * 0.587 +
+      (blue * blue) * 0.114
+    )/100
+  }
 
   class Particle {
     constructor() {
       this.x = Math.random() * canvas.width
       this.y = 0
       this.speed = 0
-      this.velocity = Math.random() * 3.5
+      this.velocity = Math.random() * 0.5
       this.size = Math.random() * 1.5 + 1
+      this.position1 = Math.floor(this.y)
+      this.position2 = Math.floor(this.x)
     }
     update() {
-      this.y += this.velocity
+      this.position1 = Math.floor(this.y)
+      this.position2 = Math.floor(this.x)
+      this.speed = mappedImage[this.position1][this.position2][0]
+      let movement = (2.5 - this.speed) + this.velocity
+      this.y += movement
       if(this.y >= canvas.height) {
         this.y = 0
         this.x = Math.random() * canvas.width
@@ -49,12 +78,14 @@ myImage.addEventListener('load', () => {
   init()
   
   function animate() {
-    ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height)
+    // ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height)
     ctx.globalAlpha = 0.05
     ctx.fillStyle = 'rgb(0, 0, 0)'
     ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.globalAlpha = 0.2
     for(let i = 0; i < particlesArray.length; i++) {
       particlesArray[i].update()
+      ctx.globalAlpha = particlesArray[i].speed * 0.5
       particlesArray[i].draw()
     }
     requestAnimationFrame(animate)
